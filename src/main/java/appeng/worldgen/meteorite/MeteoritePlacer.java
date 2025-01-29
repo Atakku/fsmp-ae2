76990dac0d6dd9bd68c6afda0fb4f554e067a919
Atakku <atakkudev@gmail.com>
2025-01-29 11:41:48 +0100

    docs & height tweaks

--- src/main/java/appeng/worldgen/meteorite/MeteoritePlacer.java
+++ src/main/java/appeng/worldgen/meteorite/MeteoritePlacer.java
@@ -165,7 +165,7 @@ public final class MeteoritePlacer {
     }
 
     private void placeCrater() {
-        final int maxY = 255;
+        final int maxY = 380;
         MutableBlockPos blockPos = new MutableBlockPos();
         BlockState filler = craterType.getFiller().defaultBlockState();
 
