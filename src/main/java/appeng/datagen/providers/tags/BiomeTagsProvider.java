8a8b74c2ce1770459c0f49e392140fe1825defc4
Atakku <atakkudev@gmail.com>
2025-01-29 10:50:24 +0100

    spawn meteorites in end instead

--- src/main/java/appeng/datagen/providers/tags/BiomeTagsProvider.java
+++ src/main/java/appeng/datagen/providers/tags/BiomeTagsProvider.java
@@ -34,6 +34,6 @@ public class BiomeTagsProvider extends net.minecraft.data.tags.BiomeTagsProvider
 
     @Override
     protected void addTags(HolderLookup.Provider provider) {
-        tag(MeteoriteStructure.BIOME_TAG_KEY).addOptionalTag(ConventionalBiomeTags.IN_OVERWORLD.location());
+        tag(MeteoriteStructure.BIOME_TAG_KEY).addOptionalTag(ConventionalBiomeTags.IN_THE_END.location());
     }
 }
